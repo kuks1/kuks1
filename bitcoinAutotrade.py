424a10c82e098ae44dcc6da7568ae6348ff7795b
kuks1 <88133777+kuks1@users.noreply.github.com>
2023-06-06 17:49:50 +0900

    bitcoinAutotrade.py

--- bitcoinAutotrade.py
+++ bitcoinAutotrade.py
@@ -5,38 +5,38 @@ import datetime
 access = "T8Gczn8HRGcr4M8c4ASmxQOV29DI5deTmukhh9I5"
 secret = "ppeVs1imxFRt18DeLCGJKyQ1IFOHCekOY6Oxkfxl"
 
-def get_target_price(ticker, k):
+def get_target_price(bitcoin, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
     df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
     target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
-def get_start_time(ticker):
+def get_start_time(bitcoin):
     """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    df = pyupbit.get_ohlcv(bitcoin, interval="day", count=1)
     start_time = df.index[0]
     return start_time
 
-def get_ma15(ticker):
+def get_ma15(bitcoin):
     """15일 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
+    df = pyupbit.get_ohlcv(bitcoin, interval="day", count=15)
     ma15 = df['close'].rolling(15).mean().iloc[-1]
     return ma15
 
-def get_balance(ticker):
+def get_balance(bitcoin):
     """잔고 조회"""
     balances = upbit.get_balances()
     for b in balances:
-        if b['currency'] == ticker:
+        if b['currency'] == bitcoin:
             if b['balance'] is not None:
                 return float(b['balance'])
             else:
                 return 0
     return 0
 
-def get_current_price(ticker):
+def get_current_price(bitcoin):
     """현재가 조회"""
-    return pyupbit.get_orderbook(ticker=ticker)["orderbook_units"][0]["ask_price"]
+    return pyupbit.get_orderbook(bitcoin=bitcoin)["orderbook_units"][0]["ask_price"]
 
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
